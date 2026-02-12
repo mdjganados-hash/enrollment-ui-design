f5db2332c027af91a8e9ecf88779d622120a72ec
mdjganados <mdjganados@tip.edu.ph>
2026-02-12 17:27:17 +0800

    style: implement css grid layout and expand form sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 function App() {
   return (
     <div>
@@ -5,45 +7,107 @@ function App() {
 
       <form>
 
+        {/* PERSONAL INFORMATION */}
         <h2>Personal Information</h2>
-
         <fieldset>
           <legend>Student Identity</legend>
 
-          <label>First Name</label>
-          <input type="text" /><br /><br />
+          <div className="grid-4">
+            <div>
+              <label>First Name</label>
+              <input type="text" />
+            </div>
 
-          <label>Middle Name</label>
-          <input type="text" /><br /><br />
+            <div>
+              <label>Middle Name</label>
+              <input type="text" />
+            </div>
 
-          <label>Last Name</label>
-          <input type="text" /><br /><br />
+            <div>
+              <label>Last Name</label>
+              <input type="text" />
+            </div>
 
-          <label>Suffix</label>
-          <input type="text" /><br /><br />
+            <div>
+              <label>Suffix</label>
+              <input type="text" />
+            </div>
+          </div>
 
-          <label>Date of Birth</label>
-          <input 
-            type="date" 
-            onKeyDown={(e) => e.preventDefault()} 
-          /><br /><br />
+          <br />
 
-          <label>Gender</label>
-          <select>
-            <option>Male</option>
-            <option>Female</option>
-            <option>Non-binary</option>
-          </select><br /><br />
+          <div className="grid-3">
+            <div>
+              <label>Date of Birth</label>
+              <input type="date" onKeyDown={(e) => e.preventDefault()} />
+            </div>
 
-          <label>Nationality</label>
-          <select>
-            <option>Filipino</option>
-            <option>American</option>
-            <option>Other</option>
-          </select><br /><br />
+            <div>
+              <label>Gender</label>
+              <select>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Non-binary</option>
+              </select>
+            </div>
+
+            <div>
+              <label>Nationality</label>
+              <select>
+                <option>Filipino</option>
+                <option>American</option>
+                <option>Other</option>
+              </select>
+            </div>
+          </div>
+
+          <br />
 
           <label>Religion</label>
           <input type="text" />
+
+        </fieldset>
+
+        {/* CONTACT DETAILS */}
+        <h2>Contact Details</h2>
+        <fieldset>
+          <legend>Communication Information</legend>
+
+          <div className="grid-3">
+            <div>
+              <label>Email</label>
+              <input type="email" />
+            </div>
+
+            <div>
+              <label>Mobile</label>
+              <input type="tel" />
+            </div>
+
+            <div>
+              <label>Landline</label>
+              <input type="tel" />
+            </div>
+          </div>
+
+        </fieldset>
+
+        {/* ENROLLMENT */}
+        <h2>Enrollment Choices</h2>
+        <fieldset>
+          <legend>Program Selection</legend>
+
+          <h3>Academic Level</h3>
+          <div className="radio-group">
+            <label><input type="radio" name="level" /> Undergraduate</label>
+            <label><input type="radio" name="level" /> Graduate</label>
+          </div>
+
+          <h3>Campus</h3>
+          <div className="radio-group">
+            <label><input type="radio" name="campus" /> Manila</label>
+            <label><input type="radio" name="campus" /> Quezon City</label>
+          </div>
 
         </fieldset>
 
